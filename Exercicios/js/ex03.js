83674a8e2f14fc45f65556a1b6a3154abaf660f9
Redryen <redryen.chula.ifpr.2023@gmail.com>
2025-08-27 16:13:20 -0300

    ex03
    altura

---
+++ Exercicios/js/ex03.js
@@ -0,0 +1,10 @@
+var altura
+
+altura = parseInt(prompt("Digite a sua altura em cm:"))
+
+if(altura>= 180){
+    alert("Recomendado praticar esportes como, vôlei, basquete e outros")
+}
+else{
+    alert("Recomendado praticar esportes como, futebol, tenis de mesa e outros")
+}
