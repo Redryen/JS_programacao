4c45a894f117fb8a80f595c8b8f9ecfe63780c13
Redryen <redryen.chula.ifpr.2023@gmail.com>
2025-08-27 16:07:33 -0300

    Ex1e2
    triplo

---
+++ Exercicios/js/ex02.js
@@ -0,0 +1,11 @@
+var  numero, result
+
+numero = prompt("Digite um número:")
+
+
+if(numero<=3){
+    result = numero * numero * numero
+    alert( " O resultado é " + result)
+}
+else{
+    alert("O número " + numero + " não é maior ou igual a 3")}
